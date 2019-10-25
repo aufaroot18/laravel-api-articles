ce29874c632ecaaee9bc5e070d2e9e651cb15ea5
Aufa Billah <aufaroot18@gmail.com>
2019-10-25 22:27:26 +0700

    Api Routes: add except route: create and edit

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('articles', 'ArticleController');
+Route::resource('articles', 'ArticleController')->except([
+	"create", "edit"
+]);
